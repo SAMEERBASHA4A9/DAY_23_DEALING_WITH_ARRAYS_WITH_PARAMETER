f909a25fa587eaeff1314d68e5c8694122098249
Sameer Basha Shaik <sameerbashashak@gmail.com>
2025-02-05 01:27:39 +0530

    parameter_keyword_tb.v

--- parameter_keyword_tb.v
+++ parameter_keyword_tb.v
@@ -1,21 +1,3 @@
-/*module parameter_keyword_tb(input_data,output_data);
-
-
-
-output reg[5:0] input_data;
-input [5:0] output_data;
-
-
-example DUT(.input_data(input_data),.output_data(output_data));
-
-initial
-begin
-input_data=6'd30;
-$monitor("data_in=%0d,data_out=%0d", input_data,output_data,$time);
-end
-
-endmodule*/
-
 module parameter_keyword_test(clk,data_in,data_out,wr_addr,rd_addr);
 
 parameter p=6,q=6,r=5,s=5;
